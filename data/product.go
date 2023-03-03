28c687a5516909e6ad035dac8ba8f3dcb1749ef9
Marko <markolitovchenko24@gmail.com>
2023-03-02 20:42:32 -0500

    Updated product type

--- data/product.go
+++ data/product.go
@@ -1,28 +1,27 @@
 package data
 
 type Product struct {
-	Code        string `json:"code"`
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	Brand       string `json:"brand"`
-	Link        string `json:"link"`
-	//ImageAssets []imageAsset `json:"imageAssets"`
-	PackageSize string `json:"packageSize"`
-	Shoppable   bool   `json:"shoppable"`
-	// price wrapper contains price object inside it
-	//Price PriceWrapper `json:"price"`
-	//PricingUnits []PricingUnit `json:"pricingUnits"`
-	// Badges []Badge `json:"badges"`
-	StockStatus       string `json:"stockStatus"`
-	ArticleNumber     string `json:"articleNumber"`
-	Uom               string `json:"uom"`
-	Taxes             string `json:"taxes"`
-	Fees              string `json:"fees"`
-	SellerId          string `json:"sellerId"`
-	SellerName        string `json:"sellerName"`
-	IsVariant         bool   `json:"isVariant"`
-	OfferType         string `json:"offerType"`
-	HasMultipleOffers bool   `json:"hasMultipleOffers"`
-	//Aisle Aisle `json:"aisle"`
-	Sponsored bool `json:"sponsored"`
+	Code              string       `json:"code"`
+	Name              string       `json:"name"`
+	Description       string       `json:"description"`
+	Brand             string       `json:"brand"`
+	Link              string       `json:"link"`
+	ImageAssets       []ImageAsset `json:"imageAssets"`
+	PackageSize       string       `json:"packageSize"`
+	Shoppable         bool         `json:"shoppable"`
+	Prices            Prices       `json:"price"`
+	PricingUnits      PricingUnits `json:"pricingUnits"`
+	Badges            Badges       `json:"badges"`
+	StockStatus       string       `json:"stockStatus"`
+	ArticleNumber     string       `json:"articleNumber"`
+	Uom               string       `json:"uom"`
+	Taxes             string       `json:"taxes"`
+	Fees              string       `json:"fees"`
+	SellerId          string       `json:"sellerId"`
+	SellerName        string       `json:"sellerName"`
+	IsVariant         bool         `json:"isVariant"`
+	OfferType         string       `json:"offerType"`
+	HasMultipleOffers bool         `json:"hasMultipleOffers"`
+	Aisle             Aisle        `json:"aisle"`
+	Sponsored         bool         `json:"sponsored"`
 }
