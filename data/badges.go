c11c6519e11cad374b6a571f522f8967d78ea300
Marko <markolitovchenko24@gmail.com>
2023-03-02 20:39:22 -0500

    Added badges type

---
+++ data/badges.go
@@ -0,0 +1,8 @@
+package data
+
+type Badges struct {
+	TextBadge    string `json:"textBadge"`
+	LoyaltyBadge string `json:"loyaltyBadge"`
+	DealBadge    string `json:"dealBadge"`
+	NewItemBadge string `json:"newItemBadge"`
+}
