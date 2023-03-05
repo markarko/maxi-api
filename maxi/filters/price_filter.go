ebd7b966cff12d34eb9e703d264228737052d0fc
Marko <markolitovchenko24@gmail.com>
2023-03-04 22:08:32 -0500

    Added function that gets products with a bigger price than the one passed in

---
+++ maxi/filters/price_filter.go
@@ -0,0 +1,15 @@
+package filters
+
+import (
+	"github.com/markarko/maxi-api/data"
+)
+
+func GetProductsWithBiggerPrice(products data.Products, price float64) data.Products {
+	var filteredProducts data.Products
+	for _, product := range products {
+		if float64(product.Prices.Price.Value) > price {
+			filteredProducts = append(filteredProducts, product)
+		}
+	}
+	return filteredProducts
+}
