5d290533c07945cbcfa65f124405d0e8d88a32e7
Marko <markolitovchenko24@gmail.com>
2023-03-04 21:56:19 -0500

    Added graceful server shutdown and products route

--- main.go
+++ main.go
@@ -1,9 +1,44 @@
 package main
 
 import (
-	"github.com/markarko/maxi-api/maxi"
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/markarko/maxi-api/handlers"
 )
 
 func main() {
-	maxi.GetAllProducts("eggs")
+	l := log.New(os.Stdout, "maxi-api ", log.LstdFlags)
+	ph := handlers.NewProducts(l)
+
+	sm := http.NewServeMux()
+	sm.HandleFunc("/products", ph.ServeHTTP)
+
+	s := &http.Server{
+		Addr:         "localhost:8080",
+		Handler:      sm,
+		ErrorLog:     l,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <-sigChan
+	l.Println("Received terminate, graceful shutdown", sig)
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tc)
 }
