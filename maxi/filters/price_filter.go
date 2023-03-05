1b29e9195d8bb06852d2ed748d2f8aabf49207f3
Marko <markolitovchenko24@gmail.com>
2023-03-04 22:22:20 -0500

    Refactored price filtering functions

--- maxi/filters/price_filter.go
+++ maxi/filters/price_filter.go
@@ -4,30 +4,35 @@ import (
 	"github.com/markarko/maxi-api/data"
 )
 
-func GetProductsWithBiggerPrice(products data.Products, price float64) data.Products {
-	var filteredProducts data.Products
-	for _, product := range products {
-		if float64(product.Prices.Price.Value) > price {
-			filteredProducts = append(filteredProducts, product)
-		}
-	}
-	return filteredProducts
+type PriceFilter int8
+
+const (
+	BIGGER PriceFilter = iota
+	SMALLER
+	EQUAL
+)
+
+func GetProductsWithBiggerPrice(products data.Products, price float32) data.Products {
+	return GetProductsAccordingToPrice(products, price, BIGGER)
 }
 
-func GetProductsWithSmallerPrice(products data.Products, price float64) data.Products {
-	var filteredProducts data.Products
-	for _, product := range products {
-		if float64(product.Prices.Price.Value) < price {
-			filteredProducts = append(filteredProducts, product)
-		}
-	}
-	return filteredProducts
+func GetProductsWithSmallerPrice(products data.Products, price float32) data.Products {
+	return GetProductsAccordingToPrice(products, price, SMALLER)
 }
 
-func GetProductsWithEqualPrice(products data.Products, price float64) data.Products {
+func GetProductsWithEqualPrice(products data.Products, price float32) data.Products {
+	return GetProductsAccordingToPrice(products, price, EQUAL)
+}
+
+func GetProductsAccordingToPrice(products data.Products, price float32, filter PriceFilter) data.Products {
 	var filteredProducts data.Products
 	for _, product := range products {
-		if float64(product.Prices.Price.Value) == price {
+
+		priceToCompare := float32(product.Prices.Price.Value)
+
+		if filter == BIGGER && priceToCompare > price ||
+			filter == SMALLER && priceToCompare < price ||
+			filter == EQUAL && priceToCompare == price {
 			filteredProducts = append(filteredProducts, product)
 		}
 	}
