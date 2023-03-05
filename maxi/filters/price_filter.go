331acd9dec97ef1450776cdcfacdca84326c58a4
Marko <markolitovchenko24@gmail.com>
2023-03-04 22:09:17 -0500

    Added function that gets products with smaller prices than the one passed in

--- maxi/filters/price_filter.go
+++ maxi/filters/price_filter.go
@@ -13,3 +13,13 @@ func GetProductsWithBiggerPrice(products data.Products, price float64) data.Prod
 	}
 	return filteredProducts
 }
+
+func GetProductsWithSmallerPrice(products data.Products, price float64) data.Products {
+	var filteredProducts data.Products
+	for _, product := range products {
+		if float64(product.Prices.Price.Value) < price {
+			filteredProducts = append(filteredProducts, product)
+		}
+	}
+	return filteredProducts
+}
