1a507b9b4312367c1c71b62a9d63a23955260df4
Marko <markolitovchenko24@gmail.com>
2023-03-04 21:58:21 -0500

    Moved tojson and fromjson methods to productscontainer

--- data/products_container.go
+++ data/products_container.go
@@ -1,6 +1,9 @@
 package data
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"io"
+)
 
 type ProductsContainer struct {
 	query              json.RawMessage `json:"-"`
@@ -17,4 +20,14 @@ type ProductsContainer struct {
 	SearchVariation    json.RawMessage `json:"-"`
 	Dym                json.RawMessage `json:"-"`
 	OfferTypes         json.RawMessage `json:"-"`
+}
+
+func (pc *ProductsContainer) ToJSON(w io.Writer) error {
+	e := json.NewEncoder(w)
+	return e.Encode(pc)
+}
+
+func (pc *ProductsContainer) FromJSON(r io.Reader) error {
+	d := json.NewDecoder(r)
+	return d.Decode(pc)
 }
