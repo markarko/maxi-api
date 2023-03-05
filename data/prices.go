c66b11735bd4cc6b68a943255d5e061acfb6802e
Marko <markolitovchenko24@gmail.com>
2023-03-04 21:55:34 -0500

    Replaced wasprice type to wasprice struct

--- data/prices.go
+++ data/prices.go
@@ -2,6 +2,6 @@ package data
 
 type Prices struct {
 	Price            Price             `json:"price"`
-	WasPrice         string            `json:"wasPrice"`
+	WasPrice         WasPrice          `json:"wasPrice"`
 	ComparisonPrices []ComparisonPrice `json:"comparisonPrices"`
 }
